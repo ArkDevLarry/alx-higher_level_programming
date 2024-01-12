9ed716f75c3288ad37b2032c7d955a29624e677f
Quadri Akerele <akereleayomide106@gmail.com>
2024-01-12 00:27:53 -0800

    Print combination wo duplicates

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for num1 in range (0,10):
+    for num2 in range (0,10):
+        if str(num1) + str(num2) == "89":
+            print("{}{}".format(num1, num2))
+        elif not (num1 >= num2):
+            print("{}{}".format(num1, num2), end=", ")
