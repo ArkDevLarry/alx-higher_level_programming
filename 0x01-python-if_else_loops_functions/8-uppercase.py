a926a1b73487aa4ad3bbb18270089ca652aa3ce2
Quadri Akerele <akereleayomide106@gmail.com>
2024-01-12 09:00:05 -0800

    Convert lower to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     """Print a string in uppercase."""
-    for char in str:
-        if ord("a") <= ord(char) <= ord("z"):
-            char = chr((ord(char) - ord("a")) + ord("A"))
-        print("{}".format(char), end="")
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
