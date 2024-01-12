e1990abe70b056616940ed86c18c5fc7aa418a05
Quadri Akerele <akereleayomide106@gmail.com>
2024-01-12 00:13:15 -0800

    Print comb 0-99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if num > 9:
-        print("{}".format(num), end=", ")
+    if num < 99:
+        print("{:02}".format(num), end=", ")
     else:
-        print("0{}".format(num), end=", ")
+        print("{}".format(num))
