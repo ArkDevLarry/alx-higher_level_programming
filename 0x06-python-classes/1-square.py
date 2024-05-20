1adc619fdaeef94c301b8c5eddbda1506e183585
Akerele Quadri <akereleayomide106@gmail.com>
2024-05-20 16:44:22 +0100

    Make files executable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""An empty class Square that defines a square."""
+
+
+class Square:
+    """A square is a shape that has 4 equal sides."""
+    
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
