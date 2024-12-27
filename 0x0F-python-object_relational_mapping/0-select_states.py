7a52f2229d0a8c4f7443930cce60a985bf987594
Akerele Quadri <akereleayomide106@gmail.com>
2024-12-27 19:57:03 +0100

    Msg

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+This script lists all states from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -9,8 +8,7 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
-    from the database.
+    Access to the database and get the states from the database.
     """
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
@@ -23,4 +21,3 @@ if __name__ == '__main__':
 
     for row in rows_selected:
         print(row)
-    db_cursor.close()
