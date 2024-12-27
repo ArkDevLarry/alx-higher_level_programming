d4a4ef321d12995d7517fc609c73ab43f56eb374
Akerele Quadri <akereleayomide106@gmail.com>
2024-12-27 19:55:15 +0100

    Msg

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,10 +14,12 @@ if __name__ == '__main__':
     """
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
     db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    db_cursor.execute("SELECT * FROM states")
+
     rows_selected = db_cursor.fetchall()
+
     for row in rows_selected:
         print(row)
-    db_cursor.close()
-    rows_selected.close()
