a5bc52cf05f937c6b282b69ff67b9026c002b5d0
Akerele Quadri <akereleayomide106@gmail.com>
2025-01-01 15:42:40 +0100

    Push again

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,7 @@ This script defines a City class
 to work with MySQLAlchemy ORM.
 """
 from model_state import Base, State
-from SQLAlchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
