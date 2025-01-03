126130400bcf81a909898f35cb9e91a9dbc3d615
Akerele Quadri <akereleayomide106@gmail.com>
2025-01-03 06:56:31 +0100

    push

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-try {
-    const fArg = fs.readFileSync(process.argv[2], 'utf8');
-    const sArg = fs.readFileSync(process.argv[3], 'utf8');
-    fs.writeFileSync(process.argv[4], fArg + sArg);
-} catch (error) {
-    console.error('Error:', error.message);
-}
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
